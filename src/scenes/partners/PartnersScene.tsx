26fdb8b03f7d6ad8895e5d8ac861a507fdeb50e8
Mitch Pierias <mitch@pierias.com>
2025-01-20 01:06:53 +1000

    feat: basic partners layout

--- src/scenes/partners/PartnersScene.tsx
+++ src/scenes/partners/PartnersScene.tsx
@@ -1,6 +1,10 @@
-export const PartnersScene = () => {
+import styles from "./PartnersScene.module.css";
+
+export const PartnersScene = ({
+  testID = "partners",
+}: Readonly<Partial<Common.ComponentProps>>) => {
   return (
-    <div className="grid col-content p-5">
+    <div data-testid={testID} className={styles.frame}>
       <h1>Partnerships</h1>
       <p>
         Promotional raffles are marketing tools used by businesses to increase
