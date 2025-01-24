cfc19ad41b67631f596e5bc86ae1b16f062f2b46
Mitch Pierias <mitch@pierias.com>
2025-01-24 20:51:47 +1000

    feat: locked down program version

--- src/state/subscription/utils.ts
+++ src/state/subscription/utils.ts
@@ -48,7 +48,7 @@ export const mapMemberFromStruct = (
     totalAmount: account.totalAmount.div(decimalFactor).toNumber(),
     totalMatured: account.totalMatured.div(decimalFactor).toNumber(),
     totalReleased: account.totalReleased.div(decimalFactor).toNumber(),
-    totalEntries: 0,
+    totalEntries: account.entries.toNumber(),
     timeCreated: dateFromBigNumber(account.timeCreated),
     timeRewarded: isInitialAccount(account)
       ? null
