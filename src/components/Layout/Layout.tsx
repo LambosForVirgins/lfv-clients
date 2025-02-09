cdb31ad200efabfd077becfb95b9132185576bfb
Mitch Pierias <mitch@pierias.com>
2025-02-09 14:06:24 +1000

    chore: cleanup unused

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { ErrorBoundary } from "@/components/ErrorBoundary/errors";
 import { Header } from "../Header/Header";
-import { PromoSection } from "../PromoSection/PromoSection";
 import { Subscribe } from "../Subscribe/Subscribe";
 import { Footer } from "../Footer/Footer";
 import { Disclaimers } from "../Disclaimers/Disclaimers";
@@ -21,7 +20,17 @@ export const Layout = ({
       <section data-testid={`${testID}.content`} className={styles.main}>
         <ErrorBoundary fallback={<h1>Error</h1>}>{children}</ErrorBoundary>
       </section>
-      <PromoSection testID={`${testID}.promo`} className={styles.promo} />
+      <div data-testid={testID} className={styles.promo}>
+        <div className={styles.content}>
+          {/* <img
+          src={"/images/banner.png"}
+          alt={"banner"}
+          height={120}
+          className="justify-self-center absolute"
+          style={{ bottom: 0, right: 0, maxWidth: "100%" }}
+        /> */}
+        </div>
+      </div>
       <Subscribe testID={`${testID}.subscribe`} className={styles.full} />
       {isEnabled("footer") && (
         <Footer testID={`${testID}.footer`} className={styles.full} />
