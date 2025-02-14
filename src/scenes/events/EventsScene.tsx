e3727c68e3e438ceaea4048d2f2573aef3458e34
Mitch Pierias <mitch@pierias.com>
2025-02-14 23:13:53 +1000

    feat: event types

--- src/scenes/events/EventsScene.tsx
+++ src/scenes/events/EventsScene.tsx
@@ -1,11 +1,36 @@
 import { EventCard } from "@/components/EventCard/EventCard";
 import styles from "./EventsScene.module.css";
 
-const EVENTS = [
+enum EventStatus {
+  Open = "open",
+  Upcoming = "upcoming",
+  Ending = "ending",
+  Closed = "closed",
+  Cancelled = "cancelled",
+}
+
+interface Event {
+  id: string;
+  title: string;
+  status: EventStatus;
+  startTime: Date;
+  location: {
+    id: string;
+    city: string;
+    country: string;
+  };
+  pricing: { currency: string; amount: number }[];
+  attendees?: number;
+  capacity?: number;
+  media: { id: string; src: string; alt: string }[];
+  links: { key: string; label: string; url: string }[];
+}
+
+const EVENTS: Event[] = [
   {
     id: "4e55d4f4-9bc0-44d0-9fe3-108ff0a0fafb",
     title: "Consensus Hong Kong 2025 Afterparty",
-    status: "closed",
+    status: EventStatus.Closed,
     startTime: new Date("2025-02-14T18:00:00"),
     location: {
       id: "d331401e-9b4f-4101-a8ad-cba261fb5a32",
@@ -13,6 +38,8 @@ const EVENTS = [
       country: "China",
     },
     pricing: [{ currency: "USD", amount: 14 }],
+    attendees: 500,
+    capacity: 1000,
     media: [
       {
         id: "fe7d5949-412f-42a2-930d-4a08a5d0b9d3",
@@ -31,7 +58,7 @@ const EVENTS = [
   {
     id: "7d8e5ca6-fa4d-4d22-a6d8-b55a0828e694",
     title: "Amsterdam Blockchain Week",
-    status: "upcoming",
+    status: EventStatus.Upcoming,
     startTime: new Date("2025-03-13T22:00:00"),
     location: {
       id: "2b4bab72-a5f4-465f-8e3b-b5b263808212",
@@ -57,7 +84,7 @@ const EVENTS = [
   {
     id: "5e27d9b1-1062-48f0-a761-ef26f740b5f4",
     title: "Consensus Toronto 2025 Afterparty",
-    status: "upcoming",
+    status: EventStatus.Upcoming,
     startTime: new Date("2025-04-14T21:00:00"),
     location: {
       id: "43986c84-f71d-4ffb-9656-74e45593b2c6",
@@ -83,14 +110,17 @@ const EVENTS = [
   {
     id: "f4f66e23-4adf-40be-aa7d-db6c1307b8dd",
     title: "Breakpoint 2025",
-    status: "upcoming",
+    status: EventStatus.Upcoming,
     startTime: new Date("2025-12-11T19:30:00"),
     location: {
       id: "bb3104dd-b91c-49b6-992a-9267f963346f",
       city: "Toronto",
       country: "Canada",
     },
-    pricing: [{ currency: "USD", amount: 14 }],
+    pricing: [
+      { currency: "VIRGIN", amount: 500_000 },
+      { currency: "SOL", amount: 1.24 },
+    ],
     media: [
       {
         id: "80f18585-62ad-4f0c-a054-efa0ab69a1b0",
@@ -130,6 +160,9 @@ export const EventsScene = ({
         ))}
       </div>
       <h2 className={styles.header}>Invitations</h2>
+      <p className={styles.header}>
+        List of events you're attending or have previously attended.
+      </p>
     </div>
   );
 };
