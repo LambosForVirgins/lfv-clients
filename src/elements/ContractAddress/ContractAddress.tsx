bc2ddb0d4236f6c8d2fcf1fecd2a9e3ec21a1351
Mitch Pierias <mitch@pierias.com>
2025-02-18 09:06:46 +1000

    fix: copy field

--- src/elements/ContractAddress/ContractAddress.tsx
+++ src/elements/ContractAddress/ContractAddress.tsx
@@ -2,6 +2,7 @@ import { Button } from "../Buttons/Button";
 import styles from "./ContractAddress.module.css";
 import clsx from "classnames";
 import { prettyAddress } from "@/utils/string/prettyAddress";
+import { useRef, useState } from "react";
 
 interface ContractAddressProps extends Common.ComponentProps {
   label?: string;
@@ -9,9 +10,32 @@ interface ContractAddressProps extends Common.ComponentProps {
   className?: string;
 }
 
+const CONFIRMATION_DURATION = 3000;
+
 export const ContractAddress = ({ testID, ...props }: ContractAddressProps) => {
+  const timerRef = useRef<NodeJS.Timeout>();
+  const [hasCopied, setHasCopied] = useState(false);
+  // Copy the address to clipboard
+  const copyAddress = () => {
+    navigator.clipboard.writeText(props.mint);
+    setHasCopied(true);
+
+    if (timerRef.current) clearTimeout(timerRef.current);
+
+    timerRef.current = setTimeout(() => {
+      setHasCopied(false);
+    }, CONFIRMATION_DURATION);
+  };
+
   return (
-    <span data-testid={testID} className={clsx(props.className, styles.frame)}>
+    <span
+      data-testid={testID}
+      className={clsx(
+        props.className,
+        styles.frame,
+        hasCopied && styles.copied
+      )}
+    >
       {props.label && (
         <span data-testid={`${testID}.label`} className={styles.label}>
           {props.label}
@@ -28,6 +52,8 @@ export const ContractAddress = ({ testID, ...props }: ContractAddressProps) => {
         testID={`${testID}.copy`}
         size={"small"}
         data-short={"Copy"}
+        disabled={hasCopied}
+        onClick={copyAddress}
         className={styles.button}
       >
         <span>Copy address</span>
