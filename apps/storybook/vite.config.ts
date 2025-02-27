f55619a995649a889f65b957a36f7b45a0c84ef2
Mitch Pierias <mitch@pierias.com>
2025-02-27 21:09:50 +1000

    feat: slot collections

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-console.log(path.join(process.cwd(), "src"));
+const pathFromRoot = (...args: string[]) =>
+  path.join(process.cwd(), "../..", ...args);
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: pathFromRoot("public"),
   resolve: {
     alias: {
       "@": path.join(process.cwd(), "../..", "src"),
